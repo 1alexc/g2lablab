fb9b22d2609f399ea200d82e27925361a4c1d57c
Alexander Carr <1alexander.carr@gmail.com>
2024-11-29 22:55:33 +0000

    quickstart_check.js weviate example working, returning true in terminal

---
+++ benefits-chatbot/quickstart_check.js
@@ -0,0 +1,19 @@
+import weaviate from 'weaviate-client';
+import dotenv from 'dotenv';
+
+dotenv.config();
+// Best practice: store your credentials in environment variables
+const wcdUrl = process.env.WCD_URL
+const wcdApiKey = process.env.WCD_API_KEY
+
+const client = await weaviate.connectToWeaviateCloud(
+  wcdUrl, // Replace with your Weaviate Cloud URL
+  {
+    authCredentials: new weaviate.ApiKey(wcdApiKey), // Replace with your Weaviate Cloud API key
+  }
+);
+
+var clientReadiness = await client.isReady();
+console.log(clientReadiness); // Should return `true`
+
+client.close(); // Close the client connection
